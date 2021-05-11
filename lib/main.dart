2520411cc8696822d64cf582f1f0f0e982d2f812
Marcos Sevilla <marcossevilla14@gmail.com>
2021-05-11 12:06:12 -0600

    Migrated to Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:calatolg/products.dart';
+import 'package:calatolg/bloc/products_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,47 +10,80 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Catalog(),
+    return ChangeNotifierProvider(
+      create: (_) => ProductsBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Catalog(),
+      ),
     );
   }
 }
 
-class Catalog extends StatefulWidget {
-  @override
-  _CatalogState createState() => _CatalogState();
-}
-
-class _CatalogState extends State<Catalog> {
-  var currentIndex = 0;
-
+class Catalog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of<ProductsBloc>(context);
+    final products = bloc.products;
+
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () => Navigator.of(context).push(ShoppingCartPage.go()),
           ),
         ],
       ),
-      drawer: Drawer(),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        shrinkWrap: true,
+        itemCount: products.length,
+        itemBuilder: (_, int i) {
+          final product = products[i];
+
+          return CheckboxListTile(
+            title: Text(product.name),
+            subtitle: Text("\$${product.price}"),
+            value: product.isChecked,
+            onChanged: (value) => bloc.updateProduct(value!, product),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  static Route go() {
+    return MaterialPageRoute<void>(builder: (_) => ShoppingCartPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of<ProductsBloc>(context);
+    final cart = bloc.cart;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Your Cart')),
       body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: products.length,
-                itemBuilder: (_, int i) {
-                  return ListTile(
-                    title: Text(products[i].producto),
-                    subtitle: Text("\$${products[i].precio}"),
-                  );
-                }),
+        children: [
+          ListView.separated(
+            shrinkWrap: true,
+            itemCount: cart.length,
+            itemBuilder: (_, index) {
+              final product = cart[index];
+
+              return ListTile(
+                title: Text(product.name),
+                trailing: Text('\$${product.price}'),
+              );
+            },
+            separatorBuilder: (_, __) => Divider(),
           ),
+          Text('Total: \$${bloc.total}'),
         ],
       ),
     );
